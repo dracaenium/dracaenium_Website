4969d1c226a0ba6ae089479750f738216038de77
Dracaenium <Dracaenium@gmail.com>
2026-03-28 21:18:30 +0100

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Providers } from "@/components/providers/Providers";
 
 export const metadata: Metadata = {
   title: "Dracaenium - Where Nature Meets Innovation",
